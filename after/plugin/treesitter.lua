4b6ae44f480e6430139314dea999e4f6089d7a1e
MuttonTheLast <muttonthelast@gmail.com>
2026-07-18 00:01:28 +0330

    Upldate

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,4 @@
-
-
-require'nvim-treesitter'.install { 'rust', 'javascript', 'c', 'cpp', 'css', 'lua',
-    'luadoc', 'luau', 'luap','c_sharp', 'hlsl', 'glsl',  'html', 'json', --'jsonc', not support
-    'json5', 'jsonnet', 'xml', 'wxml', 'yaml', 'cmake','java', 'javadoc', 'latex',
-    'python', 'sql', 'htmldjango', 'query', 'zig'}:wait(3000000)
-
+require 'nvim-treesitter'.install { 'rust', 'javascript', 'c', 'cpp', 'css', 'lua',
+    'luadoc', 'luau', 'luap', 'c_sharp', 'hlsl', 'glsl', 'html', 'json', --'jsonc', not support
+    'json5', 'jsonnet', 'xml', 'wxml', 'yaml', 'cmake', 'java', 'javadoc', 'latex',
+    'python', 'sql', 'htmldjango', 'query', 'zig' }:wait(3000000)
